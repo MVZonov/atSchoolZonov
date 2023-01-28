6c6ddd10142cdfad91d4a3d7071f00a9833885a2
Zonov Matvey <zonov90@mail.ru>
2023-01-28 15:09:15 +0300

    Загрузка дз к уроку 43.

--- sdk/HwLesson39/Task2/Summa.java
+++ sdk/HwLesson39/Task2/Summa.java
@@ -1,11 +1,5 @@
 package HwLesson39.Task2;
 
-/*Создать класс Summa, в нем cоздать и перегрузить метод sum
-- возвращающий сумму int от 2х переменных int
-- возвращающий сумму double от 3х переменных int
-- возвращающий сумму double от 2х и от 3х переменных double
-- возвращающий сумму double от 1 int и от 2х переменных double
-- возвращающий double из 2х переменных типа Integer*/
 public class Summa {
     public int sum(int a, int b) {
         return a + b;
