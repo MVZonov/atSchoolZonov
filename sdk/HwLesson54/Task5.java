633b079c6e2156ed0f6365234af7e780d3d3c149
Zonov Matvey <zonov90@mail.ru>
2023-02-14 23:48:44 +0300

    Загурзка 1, 2 задания к ДЗ урока 58.

--- sdk/HwLesson54/Task5.java
+++ sdk/HwLesson54/Task5.java
@@ -12,7 +12,14 @@ public class Task5 {
 //2)
 //    Входная строка: "1,234E567890"
 //    Вывод: Строка не является целым числом
-
+public static void main(String[] args) {
+    String test = args[0];
+    if (isInteger(test) == true) {
+        System.out.println("Строка является целым числом");
+    } else {
+        System.out.println("Строка не является целым числом");
+    }
+}
     static boolean isInteger(String str) {
         boolean isOnlyDigits = true;
         for (int i = 0; i < str.length() && isOnlyDigits; i++) {
@@ -22,14 +29,4 @@ public class Task5 {
         }
         return isOnlyDigits;
     }
-
-    public static void main(String[] args) {
-        String test = args[0];
-        if (isInteger(test) == true) {
-            System.out.println("Строка является целым числом");
-        } else {
-            System.out.println("Строка не является целым числом");
-        }
-    }
-
 }
